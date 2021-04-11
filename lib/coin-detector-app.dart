5b4afc79e7de3d961161f05d714d46916383c304
Japa Alekhin Llemos <jap@alekhin.io>
2021-04-12 04:08:00 +0800

    added stream listener to app to update the title

--- lib/coin-detector-app.dart
+++ lib/coin-detector-app.dart
@@ -1,11 +1,25 @@
 import 'package:coint_detector/routes/home.dart';
 import 'package:flutter/material.dart';
 
-class CoinDetectorApp extends StatelessWidget {
+class CoinDetectorApp extends StatefulWidget {
+  @override
+  CoinDetectorAppState createState() => CoinDetectorAppState();
+}
+
+class CoinDetectorAppState extends State<CoinDetectorApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
+    return StreamBuilder(
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        String appTitle = 'Mint';
+        if (snapshot.hasData) {
+          appTitle = snapshot.data ?? 'Mint';
+        }
+        return MaterialApp(
+          title: appTitle,
+          home: HomePage(),
+        );
+      },
     );
   }
 }
